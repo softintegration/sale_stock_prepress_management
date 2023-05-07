7ddc9e264eb720d9d0e68e96048645948d174e81
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-05-07 15:43:26 +0100

    [IMP] Delivery:Change the fr translation of the field «Customer Prepress proof reference»

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.4',
+    'version': '1.0.1.5',
     'category': 'Hidden',
     'demo': [],
     'depends': ['sale_stock','sale_prepress_management'],
